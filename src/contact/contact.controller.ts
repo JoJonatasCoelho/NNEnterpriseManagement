b67dfb25e8bd76ed37e588865bab6de263d8c370
John <jonatascleytonf@gmail.com>
2025-07-14 17:49:04 -0300

    minor: added the guard in the restant controllers

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ContactService } from './contact.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiBearerAuth('access-token')
 @Controller('contact')
 export class ContactController {
   constructor(private readonly contactService: ContactService) {}
